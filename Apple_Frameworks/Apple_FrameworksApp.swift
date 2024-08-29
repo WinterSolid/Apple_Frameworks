e6291c3ba87c3c78a25eb514559ce9439c6927fa
ZakTan <Ztanksley@luc.edu>
2024-08-29 12:39:38 -0700

    Initial Commit

---
+++ Apple_Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple_Frameworks
+//
+//  Created by Zakee Tanksley on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple_Frameworks/Framework.swift
@@ -0,0 +1,16 @@
+//
+//  Framework.swift
+//  Apple_Frameworks
+//
+//  Created by ZakTan on 8/29/24.
+//
+
+import Foundation
+struct Framework {
+    let name: String
+    let iconName: String
+    let releaseYear: Int
+    let URLString: String
+    let description: String
+    
+}
