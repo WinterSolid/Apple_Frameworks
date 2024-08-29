76e1c7d466e249bf54967bc51c73a5cb425b21a4
ZakTan <Ztanksley@luc.edu>
2024-08-29 13:53:15 -0700

    Created Lazy Vertical Grid, 3 rows across, that scrolls

---
+++ Apple_Frameworks/FrameWorkGridView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Apple_Frameworks
+//
+//  Created by ZakTan on 8/29/24.
+//
+
+import SwiftUI
+
+// grid layout with adaptive columns
+//let columns = [GridItem(.adaptive(minimum: 100))]
+let columns = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible())
+]
+
+struct FrameWorkGridView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground).ignoresSafeArea()
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(mockFrameworks) { framework in
+                        VStack {
+                            Image(framework.iconName) // Display the framework icon
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100) // Adjust the size
+                            
+                            Text(framework.name) // Display the framework name
+                                .font(.headline)
+                                .multilineTextAlignment(.center)
+                                .padding(.top, 5)
+                        }
+                        .padding()
+                        .shadow(radius: 2)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Apple Frameworks")
+        }
+    }
+}
+
+#Preview {
+    FrameWorkGridView()
+}
