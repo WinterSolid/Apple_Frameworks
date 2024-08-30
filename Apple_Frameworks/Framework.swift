eab9b3d5612f5329e1ac293f385165218a096891
ZakTan <Ztanksley@luc.edu>
2024-08-30 09:26:34 -0700

    Added additional frameworks mockdata

--- Apple_Frameworks/Framework.swift
+++ Apple_Frameworks/Framework.swift
@@ -11,7 +11,6 @@ struct Framework: Identifiable {
     let id = UUID()
     let name: String
     let iconName: String?
-    let releaseYear: Int
     let URLString: String
     let description: String
 }
@@ -19,69 +18,67 @@ struct Framework: Identifiable {
 let mockFrameworks: [Framework] = [
     Framework(name: "SwiftUI",
               iconName: "swiftui",
-              releaseYear: 2019,
               URLString: "https://developer.apple.com/xcode/swiftui/",
-              description: "A modern framework for building user interfaces across all Apple platforms using a declarative Swift syntax."),
+              description: "A modern framework for building user interfaces across all Apple platforms using a declarative Swift syntax."
+             ),
     Framework(name: "SF Symbols",
               iconName: "sf-symbols",
-              releaseYear: 2019,
               URLString: "https://developer.apple.com/sf-symbols/",
-              description: "A library of over 3,300 configurable vector-based symbols you can use in your apps."),
-    
-    
-    //    Framework(name: "CoreData",
-    //              iconName: "coredata",
-    //              releaseYear: 2005,
-    //              URLString: "https://developer.apple.com/documentation/coredata",
-    //              description: "A framework for managing the model layer of applications and providing object graph management and persistence."),
-    
+              description: "A library of over 3,300 configurable vector-based symbols you can use in your apps."
+             ),
+    Framework(name: "SiriKit",
+              iconName: "sirikit",
+              URLString: "https://developer.apple.com/documentation/sirikit",
+              description: "Integrate your app with Siri to perform tasks, handle voice commands, and provide shortcuts."
+             ),
+    Framework(name: "WeatherKit",
+              iconName: "weatherkit",
+              URLString: "https://developer.apple.com/documentation/weatherkit",
+              description: "Access detailed weather information, including forecasts, weather conditions, and more."
+              ),
+    Framework(name: "WidgetKit",
+              iconName: "widgetkit",
+              URLString: "https://developer.apple.com/documentation/widgetKit",
+              description: "Create widgets for the iOS home screen and provide glanceable information from your app."
+             ),
     Framework(name: "TestFlight",
               iconName: "test-flight",
-              releaseYear: 2014,
               URLString: "https://developer.apple.com/testflight/",
-              description: "A framework for testing beta versions of your apps and collecting feedback from users before releasing them to the App Store."),
-    
-    
+              description: "A framework for testing beta versions of your apps and collecting feedback from users before releasing them to the App Store."
+             ),
     Framework(name: "ARKit",
               iconName: "arkit",
-              releaseYear: 2017,
               URLString: "https://developer.apple.com/documentation/arkit",
-              description: "A framework that combines device motion tracking, camera scene capture, and advanced scene processing to build augmented reality experiences."),
-    
+              description: "A framework that combines device motion tracking, camera scene capture, and advanced scene processing to build augmented reality experiences."
+             ),
     Framework(name: "CoreML",
               iconName: "coreml",
-              releaseYear: 2017,
               URLString: "https://developer.apple.com/documentation/coreml",
-              description: "A machine learning framework that allows integration of trained machine learning models into apps."),
-    
+              description: "A machine learning framework that allows integration of trained machine learning models into apps."
+             ),
     Framework(name: "CarPlay",
               iconName: "carplay",
-              releaseYear: 2014,
               URLString: "https://developer.apple.com/documentation/carplay",
-              description: "A framework for integrating iOS apps with the CarPlay system, providing a safer way to use iPhone while driving."),
-    
-    
+              description: "A framework for integrating iOS apps with the CarPlay system, providing a safer way to use iPhone while driving."
+             ),
     Framework(name: "MapKit",
               iconName: "mapkit",
-              releaseYear: 2009,
               URLString: "https://developer.apple.com/documentation/mapkit",
-              description: "A framework that provides an interface for embedding maps directly into your own windows and views."),
-    
+              description: "A framework that provides an interface for embedding maps directly into your own windows and views."
+             ),
     Framework(name: "HealthKit",
               iconName: "healthkit",
-              releaseYear: 2014,
               URLString: "https://developer.apple.com/documentation/healthkit",
-              description: "A framework that provides a central repository for health and fitness data on iOS devices."),
-    
+              description: "A framework that provides a central repository for health and fitness data on iOS devices."
+             ),
     Framework(name: "SpriteKit",
               iconName: "spritekit",
-              releaseYear: 2013,
               URLString: "https://developer.apple.com/documentation/spritekit",
-              description: "A framework for developing 2D games and other graphics-intensive apps."),
-    
+              description: "A framework for developing 2D games and other graphics-intensive apps."
+             ),
     Framework(name: "Metal",
               iconName: "metal",
-              releaseYear: 2014,
               URLString: "https://developer.apple.com/metal/",
-              description: "A low-level, high-performance framework for developing graphics and compute-intensive apps.")
+              description: "A low-level, high-performance framework for developing graphics and compute-intensive apps."
+             )
 ]
---
+++ Apple_Frameworks/FrameworkModelView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Apple_Frameworks
+//
+//  Created by Zakee Tanksley on 8/29/24.
+//
+
+import SwiftUI
+
+struct FrameworkModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkModelView()
+}
